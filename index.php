8a4fbb9df7c41ed5963fd35e2a20d10fdc7831db
Robert Beaudenon <robertbeaudenon@gmail.com>
2019-04-11 15:18:30 -0400

    Layout html

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>form</title>
+</head>
+<body>
+	<h1>Practice Smarties Eating</h1>
+	<br><br>
+	<h4>Eating a box of Smarties</h4>
+	<br><br>
+	<p>How many of each color do you want?:</p>
+
+<form action="" method="post">
+	<label>Red:</label>
+	<input type="text" name="red" ><br>
+	<label>Yellow:</label>
+	<input type="text" name="yellow" ><br>
+	<label>Blue:</label>
+	<input type="text" name="blue" ><br>
+	<label>Green:</label>
+	<input type="text" name="green" ><br>
+	<input type="submit" name="submit" value="How many do i eat to date?">
+	<input type="submit" name="submit" value="I am done!!!">
+	<input type="reset" name="reset" value="Clear Choices">
+	
+</form>
+
+
+</body>
+
+</html>
